b02dd42579057738a5f084a3869885f62873de6d
robertoles <roles@cwhen-wks-38osx.changeworks.local>
2015-05-14 14:07:05 +0100

    feature: returning some basic information

--- server.rb
+++ server.rb
@@ -3,18 +3,29 @@ require 'socket'
 def handle_connection(socket)
   message = socket.gets
 
-  puts message
+  verb, address, protocol = message.split(' ')
 
-  response = "Hello World!\n"
+  response = 
+    case address
+    when '/'
+      <<-RESPONSE  
+        <h1>Available Routes:</h1>
+        <ul>
+          <li>/</li>
+          <li>/hello_world</li>
+          <li>/foobar</li>
+        </ul>
+      RESPONSE
+    end
 
   socket.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/plain\r\n" +
+               "Content-Type: text/html\r\n" +
                "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n"\
+               "Connection: close\r\n"
 
   socket.print "\r\n"
 
-  socket.print response
+  socket.print "#{response}\r\n"
 
   socket.close
 end
