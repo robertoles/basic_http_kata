327a0633041c959fc27506af34aaea08dffb15ce
robertoles <roles@cwhen-wks-38osx.changeworks.local>
2015-05-15 15:02:59 +0100

    Refactoring

--- rob/server.rb
+++ rob/server.rb
@@ -4,41 +4,83 @@ require 'cgi'
 require 'byebug'
 require 'erb'
 
+class Route
+  attr_reader :name, :path
+
+  def initialize(name, options={})
+    @name = name
+    @path = options[:path] || "/#{name}"
+  end
+
+  def for_path?(path)
+    path.match(/^#{@path}/)
+  end
+
+  def code
+    200
+  end 
+end
+
+class RouteNotFound
+  def for_path?(path)
+    true
+  end
+
+  def code
+    404
+  end
+
+  def name
+    "404"
+  end
+
+  def path
+    "unfound"
+  end
+end
+
+class Params
+
+  attr_reader :cgi
+
+  def initialize(query)
+    @cgi = CGI.parse(query.to_s)
+  end
+
+  def method_missing(name)
+    return nil unless cgi[name.to_s]
+    cgi[name.to_s][0]
+  end
+
+end
+
 def handle_connection(socket)
   message = socket.gets
 
-  verb, address, protocol = message.split(' ')
+  verb, path, protocol = message.split(' ')
 
-  uri = URI.parse(address.to_s)
+  uri = URI.parse(path.to_s)
+  params = Params.new(uri.query)
 
-  cgi = CGI.parse(uri.query.to_s)
+  routes = [
+    Route.new('hello_world'),
+    Route.new('foobar'),
+    Route.new('index', path: '/'),
+    RouteNotFound.new
+  ]
 
-  response = 
-    case address
-    when '/'
-      template = ERB.new(File.read('templates/index.html.erb'))
-      [200, template.result]
-    when '/hello_world'
-      template = ERB.new(File.read('templates/hello_world.html.erb'))
-      [200, template.result]
-    when /^\/foobar/
-      foos = Array(cgi["foos"])[0].to_s
-      bars = Array(cgi["bars"])[0].to_s
-      template = ERB.new(File.read('templates/foobar.html.erb'))
-      [200, template.result]
-    else
-      template = ERB.new(File.read('templates/404.html.erb'))
-      [404, template.result]
-    end
+  route = routes.find { |route| route.for_path?(path) }
+  
+  response = ERB.new(File.read("templates/#{route.name}.html.erb")).result(binding)
 
-  socket.print "HTTP/1.1 #{response[0]} OK\r\n" +
+  socket.print "HTTP/1.1 #{route.code} OK\r\n" +
                "Content-Type: text/html\r\n" +
-               "Content-Length: #{response[1].bytesize}\r\n" +
+               "Content-Length: #{response.bytesize}\r\n" +
                "Connection: close\r\n"
 
   socket.print "\r\n"
 
-  socket.print "#{response[1]}\r\n"
+  socket.print "#{response}\r\n"
 
   socket.close
 end
@@ -48,3 +90,4 @@ loop do
   socket = server.accept
   Thread.new { handle_connection(socket) }
 end
+
